dfc06422739ed295e3c227e99fe5520719efe3d8
strike <xiangdanqi60@gmail.com>
2018-02-20 14:50:35 +0800

    The first virsion with server and client sockets program and their executable files.

---
+++ server.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void tcp_server(){
+	//创建套接字
+	int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //将套接字和IP、端口绑定
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
+    serv_addr.sin_family = AF_INET;  //使用IPv4地址
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.1.114");  //具体的IP地址
+    serv_addr.sin_port = htons(1234);  //端口
+    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    //进入监听状态，等待用户发起请求
+    listen(serv_sock, 20);
+    printf("start listening at port 1234...\n");
+    //接收客户端请求
+    while(1)
+    {
+	    struct sockaddr_in clnt_addr;
+	    socklen_t clnt_addr_size = sizeof(clnt_addr);
+	    int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+	    //向客户端发送数据
+	    char str[] = "Hello, my name is Strike!";
+	    write(clnt_sock, str, sizeof(str));
+   		close(clnt_sock);
+    }   	   
+    //关闭套接字
+    close(serv_sock);
+
+}
+
+int main(){
+    daemon(0, 0);
+    tcp_server();
+    return 0;
+}
