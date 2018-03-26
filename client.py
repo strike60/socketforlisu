361d7101a85424bc05c7673bb94e3b22e99f88a3
lisu60 <lisu60g@gmail.com>
2018-03-26 17:40:13 +0800

    Fix string length determination, utilize client.py with argparse

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 import socket
+import argparse
+
+parser = argparse.ArgumentParser(description='Send a string to server and print response')
+parser.add_argument('string', metavar='STR', type=str, nargs=1, help='The string to send to the server')
+args = parser.parse_args()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect(('127.0.0.1', 1234))
 
-s.send('1'.encode('utf-8'))
-print(s.recv(1024))
+s.send(args.string[0].encode('utf-8'))
+print(s.recv(1024).decode('utf-8'))
 s.close()
